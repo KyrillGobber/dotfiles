89f03c849a0a93a07191bae32324213c372ca1df
Kyrill <Kyrill.gobber@cleoag.ch>
2022-10-04 00:47:53 +0200

    first steps in lua config

---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+-- Guide: https://github.com/LunarVim/Neovim-from-scratch/tree/master/lua/user
+require "user.general"
+require "user.keymaps"
+require "user.plugins"
+require "user.colorscheme"
+require "user.treesitter"
+require "user.indentline"
+require "user.coc"
